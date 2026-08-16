fb60d50b59693b7d165758a52606cee6d4570e00
Autumn <tripleumo@gmail.com>
2026-08-16 20:19:21 +0300

    feat: support for arrays in lang files

--- buildSrc/src/main/kotlin/FlattenLangTask.kt
+++ buildSrc/src/main/kotlin/FlattenLangTask.kt
@@ -1,7 +1,9 @@
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
@@ -12,6 +14,7 @@ import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import java.io.File
+import kotlin.collections.joinToString
 
 abstract class FlattenLangTask : DefaultTask() {
     @get:InputFiles
@@ -85,11 +88,10 @@ abstract class FlattenLangTask : DefaultTask() {
             return
         }
 
-        if (key.substringAfterLast('.') == marker) {
-            result.add(key.substringBeforeLast('.'), entry)
-        } else {
-            result.add(key, entry)
-        }
+        val prefix = key.removeSuffix(".$marker")
+        val value  = if (entry is JsonArray) JsonPrimitive(entry.joinToString("\n") { it.asString }) else entry
+
+        result.add(prefix, value)
     }
 }
 
